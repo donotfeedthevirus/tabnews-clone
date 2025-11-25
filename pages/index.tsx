a7180b6257a6f221af5d6f96cb377adee33b8c74
Gustavo Muniz <gusmunizsch@gmail.com>
2025-11-25 17:49:37 -0300

    feat(home): Add construction page content (really simple)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
 function Home() {
-  return <h1>Index</h1>;
+  return (
+    <h1>
+      This is the implementation of{" "}
+      <a href="https://www.tabnews.com.br" target="_blank">
+        tabnews
+      </a>{" "}
+      done from "scratch", however, currently it is{" "}
+      <span style={{ color: "red" }}>Under Construction</span>
+      {", "}
+      so please stand by!
+    </h1>
+  );
 }
 
 export default Home;
