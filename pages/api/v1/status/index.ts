433178748c3fd25c88296b0790ad010db3dac14e
Gustavo Muniz <gusmunizsch@gmail.com>
2025-12-20 00:26:13 -0300

    feat: Add ´database.js´ and ´.env´ files for easily integrating with docker Postgres image, and displaying status on route `/api/v1/status`

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database.ts";
 
-export default function status(
+export default async function status(
   req: NextApiRequest,
   res: NextApiResponse,
-): void {
+): Promise<void> {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows[0].sum);
   res.status(200).json({ status: "ok" });
 }
