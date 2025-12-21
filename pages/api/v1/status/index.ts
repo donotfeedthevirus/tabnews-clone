65218a20b2c5323124a30ca7b357d8bce2b3c3cb
Gustavo Muniz <gusmunizsch@gmail.com>
2025-12-21 20:21:27 -0300

    Add relevant return information to `/api/v1/status`route

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -5,7 +5,29 @@ export default async function status(
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0].sum);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const version = versionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE state = 'active' AND datname = $1;",
+    values: [databaseName],
+  });
+  const openConnections = openConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        open_connections: openConnections,
+      },
+    },
+  });
 }
